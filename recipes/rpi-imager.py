2f5acfc5a74b7502361a22050705fb40c0eb15c8
Philipp Wasser <mail@philippwasser.de>
2024-02-24 14:48:31 +0000

    Added rpi_imager recipe

---
+++ recipes/rpi-imager.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+import requests
+import urllib.request
+ARCH=['amd64']
+URL = "https://api.github.com/repos/raspberrypi/rpi-imager/releases/latest"
+
+load_dotenv()
+
+username = os.environ.get('GH_USER')
+token = os.environ.get('GH_TOKEN')
+auth = (username, token)
+
+def check_version(current_version):
+    resp = requests.get(URL, auth=auth)
+    data = resp.json()
+    new_version = data['tag_name'][1:]
+    print("Current version: " + current_version)
+    print("Latest version: " + new_version)
+    returndata = []
+    if new_version == current_version:
+        return (None, None)
+    else:
+        for arch in ARCH:
+            for asset in data['assets']:
+                if asset['name'] == f'rpi-imager_{new_version}_{arch}.deb':
+
+                    returndata.append(
+                        (asset['browser_download_url'], asset['name'], arch))
+
+        if len(returndata) == 0:
+            print("Error! New version but no matching assets found")
+            return (None, None)
+
+        else:
+            return (new_version, returndata)
+
+
+
+
+def build_package(version, location, data):
+    for arch in data:
+        urllib.request.urlretrieve(arch[0], location+"/"+arch[1])
